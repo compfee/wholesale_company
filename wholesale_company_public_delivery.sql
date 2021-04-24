4734c802129a2c519a272f5d3b523940fd4eb02a
compfee <55783463+compfee@users.noreply.github.com>
2021-04-24 17:00:39 +0300

    Add files via upload

--- wholesale_company_public_delivery.sql
+++ wholesale_company_public_delivery.sql
@@ -1,3 +1,3 @@
-INSERT INTO public.delivery (good_id, name, quantity, priority, id) VALUES (16, 'Горьяд', 26, 0.47, 1);
-INSERT INTO public.delivery (good_id, name, quantity, priority, id) VALUES (9, 'Лаванда', 3, 0.23, 2);
-INSERT INTO public.delivery (good_id, name, quantity, priority, id) VALUES (10, 'Вороний глаз', 6, 0.16, 3);
+INSERT INTO public.delivery (good_id, name, quantity, priority, id) VALUES (16, 'Горьяд', 26, 0.047, 1);
+INSERT INTO public.delivery (good_id, name, quantity, priority, id) VALUES (9, 'Лаванда', 3, 0.023, 2);
+INSERT INTO public.delivery (good_id, name, quantity, priority, id) VALUES (10, 'Вороний глаз', 6, 0.016, 3);
---
+++ wholesale_company_public_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users (id, name, password) VALUES (1, 'admin', 'admin');
+INSERT INTO public.users (id, name, password) VALUES (2, 'admin2', 'admin2');
+INSERT INTO public.users (id, name, password) VALUES (3, 'hello', 'world');
